0ebee9bbea5b1a82694609f5252d88c65140ad4d
Alexandre Gomes <agnsuporte@gmail.com>
2019-07-06 14:57:19 -0300

    uso do env

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,9 @@
-// const { db } = require('./.env')
+const { db } = require('./.env')
 // const connPG = process.env.DATABASE_URL
 
 module.exports = {
 	client: 'pg',
-	connection: process.env.DATABASE_URL, //db.pg,
+	connection: db.pg,
 	pool: {
 		min: 2,
 		max: 10
